70b9efb6157f5eb4de2ea593812af4e484e94646
Cheongjo <103722439+cj5311@users.noreply.github.com>
2024-11-28 18:31:45 +0900

    Delete 04.1배포 및 수익화 directory

--- 04.1배포 및 수익화/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    FRONT_URL: "https://chat-fortune-9z7.pages.dev",
-    OPENAI_API: "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",
-};
-
-
